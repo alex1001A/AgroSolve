b1456527a08ddf78a312a40a85a9f1357bbda947
alex1001A <alexiordanov1998@gmail.com>
2024-01-11 15:58:26 +0200

    burger_3

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,9 @@
+let menuBurger = document.querySelector('.menu-burger')
+let navigation = document.querySelector('.hero__navigation')
 
+menuBurger.addEventListener ('click', () => {
+    navigation.classList.toggle('active-menu')
+})
 
 
 
